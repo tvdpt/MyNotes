2e75387196057ba9fbca7bf175fb36ae8a669d4f
Thomas van der Ploeg Tygesen <Thom347j@mail.hansenberg.dk>
2023-09-06 09:57:47 +0200

    Kaptel 2 Added med Teskt fra Jake Parolta

--- MyNotes.cs
+++ MyNotes.cs
@@ -27,4 +27,49 @@ A 'string' is a sequence of characters, like ""Hello World"", ""I am a string"",
 A 'float' is a decimal number, like 1.234, 5.678, and 9.101112. 
 A 'bool' is a true or false value, like true and false. ";
 
+
+
+    public static string AIPageOne =
+@"...
+Artificial Intelligence (AI) is a fascinating field of computer science that focuses on creating systems and machines that can perform tasks typically requiring human intelligence. For beginners, it's important to start with the basics and gradually build your understanding. Here's a simplified introduction to AI for beginners:
+
+What is AI?
+AI refers to the simulation of human intelligence in machines, allowing them to perform tasks like problem-solving, learning from data, recognizing patterns, and making decisions. AI systems aim to mimic cognitive functions such as reasoning, problem-solving, and learning.
+
+Types of AI:
+Narrow or Weak AI (ANI): This type of AI is designed for specific tasks and has a limited scope. Examples include virtual personal assistants like Siri and Alexa, as well as recommendation systems on platforms like Netflix or Amazon.
+General or Strong AI (AGI): AGI refers to AI systems that possess human-like intelligence and can perform a wide range of tasks, just like humans. AGI is still a theoretical concept and hasn't been achieved yet.
+Artificial Superintelligence (ASI): ASI is an advanced form of AI that surpasses human intelligence and capabilities. It's currently in the realm of science fiction.";
+
+
+
+    public static string AIPageTwo =
+@"...
+Yo, yo, yo, it's your boy Jake Parolta, and today we're gonna talk about AI, you know, that futuristic stuff that's takin' over the world like a hot sauce on a taco. There ain't just one type of AI, my friend, there's a whole bunch of flavors, like we're at an ice cream shop of the future. So, buckle up, because we're gonna break it down, Nine-Nine style!
+
+Narrow AI (Artificial Narrow Intelligence): This one's like Terry Jeffords, super focused and specialized. It can do one thing, and it does it really well. Like, think about those spam filters in your email. They're great at catching spam but can't make you a sandwich.
+
+General AI (Artificial General Intelligence or AGI): Now, this is the real Captain Holt of AI. It's like a genius all-rounder. It can handle tasks, learn, and adapt across a whole bunch of stuff, just like Holt manages the precinct. AGI ain't here yet, though. We're still working on it.
+
+Machine Learning (ML): Think of this as Rosa Diaz. It's all about learning from data, getting better over time, and solving problems. ML is used in all sorts of stuff, from predicting crime patterns to recommending your next Netflix binge.
+
+Deep Learning: This one's like Gina Linetti – a bit eccentric, but brilliant. Deep Learning is a subset of ML and is all about neural networks. These networks mimic the way our brains work, and they're awesome at tasks like image recognition and natural language processing.
+
+Reinforcement Learning: Imagine Hitchcock and Scully, but in AI form. It's all about trial and error. The AI learns by taking actions and getting feedback. It's great for things like training game-playing bots and autonomous cars.
+
+Natural Language Processing (NLP): Boyle's domain! NLP is all about understanding and processing human language. It's how Siri understands your weird questions and chatbots like me try to sound like your favorite detectives.
+
+Computer Vision: Just like Terry loves his yogurt, computer vision is all about machines seeing and understanding images and videos. It's what powers facial recognition, self-driving cars, and even helps find perps in security footage.
+
+Expert Systems: Think of Captain Holt's vast knowledge in policing, now imagine an AI that's an expert in a specific field, like medicine or finance. These systems use rules and knowledge bases to make decisions, just like our man Holt.
+
+Robotics: This is like Terry's love for fitness, but for machines. Robots with AI can do all sorts of stuff, from assembling cars to vacuuming your house. They're like the precinct's handy-dandy detectives.
+
+Autonomous AI: It's like Gina's independence dialed up to eleven. Autonomous AI can make decisions and take actions without human intervention. Think self-driving cars or drones that deliver pizza.
+
+So there you have it, folks! The Nine-Nine guide to different types of AI. Just remember, AI is evolving faster than Boyle's food obsession, and who knows what the future holds for this exciting field! Nine-Nine! 🕺🚀";
+
+
+
+
 }
--- Program.cs
+++ Program.cs
@@ -5,17 +5,20 @@ Page FirstPage = new Page (MyNotes.VariablesPageOne);
 
 Page SecondPage = new Page (MyNotes.VariablesPageTwo);
 
-Chapter kapitel1= new Chapter("variabler");
+Chapter kapitel1= new Chapter("Variabler");
 
 kapitel1.Pages.Add(FirstPage);
 kapitel1.Pages.Add(SecondPage);
 
-Chapter kapitel2= new Chapter("Artifical intelligence");
+Page FirstPageAI = new Page (MyNotes.AIPageOne);
+Page SecondPageAI = new Page (MyNotes.AIPageTwo);
 
-kapitel2.Pages.Add(FirstPage);
-kapitel2.Pages.Add(SecondPage);
+Chapter kapitel2= new Chapter("AI");
 
-Book MyBook = new Book ("Haps");
+kapitel2.Pages.Add(FirstPageAI);
+kapitel2.Pages.Add(SecondPageAI);
+
+Book MyBook = new Book ("DIn mors noter");
 MyBook.Chapters.Add(kapitel1);
 MyBook.Chapters.Add(kapitel2);
 MyBook.Start();
