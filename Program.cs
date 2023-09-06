381eeeb4be5d9098dad9b1e3149b289db644dd2c
Thomas van der Ploeg Tygesen <Thom347j@mail.hansenberg.dk>
2023-09-06 09:31:02 +0200

    Kapitel & Book

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
 
-Page Int = new Page ("An int variable in C# is a fundamental data type used to store whole numbers. It occupies 32 bits of memory, allowing it to represent a wide range of values, both positive and negative, with a default value of 0 if not initialized. You can perform arithmetic operations on int variables, but be cautious of potential overflow issues. int variables can be declared and initialized in a single step and can be used for tasks like counting, indexing, and basic mathematical calculations. When precision is not critical, int is more efficient than other numeric types like float or double.");
+Page FirstPage = new Page (MyNotes.VariablesPageOne);
 
-Page Second = new Page ("Min anden side");
-
-Page third = new Page ("Min tredje0 side");
+Page SecondPage = new Page (MyNotes.VariablesPageTwo);
 
 Chapter kapitel1= new Chapter("variabler");
 
-kapitel1.Pages.Add(Int);
-kapitel1.Pages.Add(Second);
-kapitel1.Pages.Add(third);
+kapitel1.Pages.Add(FirstPage);
+kapitel1.Pages.Add(SecondPage);
 
-kapitel1.Read();
+Chapter kapitel2= new Chapter("Artifical intelligence");
+
+kapitel2.Pages.Add(FirstPage);
+kapitel2.Pages.Add(SecondPage);
+
+Book MyBook = new Book ("Haps");
+MyBook.Chapters.Add(kapitel1);
+MyBook.Chapters.Add(kapitel2);
+MyBook.Start();
+
+
 
 Console.ReadLine(); 
